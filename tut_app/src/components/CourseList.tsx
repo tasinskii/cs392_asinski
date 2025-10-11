2678cd02f4231499414a62b537639922bfa007bb
Timofei Asinski <tasinski@Timofeis-MacBook-Air-3.local>
2025-10-11 18:47:55 -0500

    need to fix overlaps in quarter selection

--- tut_app/src/components/CourseList.tsx
+++ tut_app/src/components/CourseList.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import Modal from './Modal'
+import type {day} from '../utilities/conflicts'
+import {updateTimes, detectOverlap} from '../utilities/conflicts'
 interface Course {
   term: string;
   number: string;
@@ -18,14 +20,15 @@ interface CourseCardProps {
   name: string;
   course: Course;
   isSelected: boolean; 
-  click: () => void
+  click: () => void;
+  isConflict: boolean;
 }
-const CourseCard = ({key, name, course, isSelected, click}: CourseCardProps) => {
-  console.log(key)
+const CourseCard = ({key, name, course, isSelected, click, isConflict}: CourseCardProps) => {
+  //console.log(key)
   return (
     <div onClick={click}>
       <div className = {`grid grid-rows-6 grid-cols-1 h-50 w-auto-fit p-2 border-2 border-gray-200 rounded-lg ${
-          isSelected ? "bg-blue-100" : ""}`}>   
+          isSelected ? "bg-blue-100" : isConflict ? "bg-red-100" : ""}`}>   
         <div className = "font-bold text-2xl text-black ">{name}</div>
         <div className="row-start-2">{course.title}</div>
         <div className="row-start-6 text-gray-500">{course.meets}</div>
@@ -37,19 +40,81 @@ const CourseCard = ({key, name, course, isSelected, click}: CourseCardProps) => 
 const CourseList = ({courses, term}: CourseListProps) => {
   const [selected, setSelected] = useState(Array(Object.entries(courses).length).fill(0));
   const [popUp, setPopUp] = useState(false);
-  const click = (i:number) => {
-    setSelected(selected => {
-      const cp = [...selected];
-      cp[i] = (cp[i] + 1) % 2;
-      return cp; 
-    });
-    
-  };
+  let init_day: day = {times: Array<boolean>(144).fill(true)}; //initially, everything is available 
+  let init_week = Array.from({ length: 5 }, () => ({
+  times: Array<boolean>(144).fill(true)
+  }))
+  const [week, setWeek] = useState(init_week);
+  const [invalidCourses, setInvalidCourses] = useState(Array<boolean>(Object.entries(courses).length).fill(false)); 
   const courseNames = Object.entries(courses).map(([name, _]) => name);
   const courseTitles = Object.entries(courses).map(([_, content]) => content.title);
   const courseMeeting = Object.entries(courses).map(([_, content]) => content.meets);
   const courseInfo = courseNames.map((_, i) => [courseNames[i], courseTitles[i], courseMeeting[i]])
-  console.log(Object.entries(courses)); 
+
+
+
+
+  const click = (i:number) => {
+    //check if overlap
+    //console.log(week)
+    const meets_data = courseMeeting[i].split(" ");
+    const days = meets_data[0];
+    const time = meets_data[1].split("-")
+    if (selected[i] == 0) {
+      console.log(detectOverlap(days, time, week))
+      if (!detectOverlap(days, time, week)) {
+        setSelected(selected => {
+        const cp = [...selected];
+        cp[i] = (cp[i] + 1) % 2;
+        return cp; 
+        });
+
+        
+        setWeek(prevWeek => {
+          const newWeek = updateTimes(days, time, prevWeek, false);
+          
+          setInvalidCourses(courseMeeting.map(meeting =>
+            detectOverlap(
+              meeting.split(" ")[0],
+              meeting.split(" ")[1].split("-"),
+              newWeek
+            )
+          ));
+          return newWeek;
+        });
+
+      } 
+    } else {
+      setSelected(selected => {
+        const cp = [...selected];
+        cp[i] = (cp[i] + 1) % 2;
+        return cp; 
+      });
+
+      setWeek(prevWeek => {
+        const newWeek = updateTimes(days, time, prevWeek, true);
+        
+        setInvalidCourses(courseMeeting.map(meeting =>
+          detectOverlap(
+            meeting.split(" ")[0],
+            meeting.split(" ")[1].split("-"),
+            newWeek
+          )
+        ));
+        return newWeek;
+      });
+
+    } 
+
+
+/*
+    setInvalidCourses(courseMeeting.map((meeting) =>  
+      detectOverlap(meeting.split(" ")[0], meeting.split(" ")[1].split("-"), week)
+    ));
+    console.log(week);*/
+  };
+
+  //console.log(Object.entries(courses)); 
   return (
     <div> 
       <button className="absolute top-2 right-2 text-gray-500 hover:text-gray-700" onClick={() => setPopUp(true)}>See selected courses</button> 
@@ -68,7 +133,7 @@ const CourseList = ({courses, term}: CourseListProps) => {
         
         {Object.entries(courses).map(([name, content], i) =>
           content.term === term ? (
-            <CourseCard key={i} name={name} course={content} isSelected={selected[i]} click={() => click(i)} />
+            <CourseCard key={i} name={name} course={content} isSelected={selected[i]} click={() => click(i)} isConflict={invalidCourses[i]} />
           ) : null
         )}    
       </div>
---
+++ tut_app/src/utilities/conflicts.tsx
@@ -0,0 +1,116 @@
+
+interface Course {
+  term: string;
+  number: string;
+  meets: string;
+  title: string;
+}
+
+interface CourseCardProps {
+  key: number;
+  name: string;
+  course: Course;
+  isSelected: boolean; 
+  click: () => void;
+  noConflict: boolean;
+}
+
+export interface day {
+  times: Array<boolean>; //ten minute increments, false is occupied, available is true
+}
+
+
+function calctime_in10mins(time: Array<string>) { 
+  let start_time = (time[0].split(":")[0] * 6) + (time[0].split(":")[1] / 10); //mins
+  let end_time = (time[1].split(":")[0] * 6) + (time[1].split(":")[1] / 10); //mins ,
+  return [start_time, end_time];
+}
+
+
+function newday(time: Array<string>, times: Array<boolean>, set_or_clear: boolean) {
+  //console.log("newday")
+  let [start_time, end_time] = calctime_in10mins(time);
+  let busy = Array<boolean>(end_time - start_time).fill(set_or_clear);
+  const newArray = [
+    ...times.slice(0, start_time),
+    ...busy,
+    ...times.slice(end_time)
+  ]
+
+  return newArray; 
+}
+
+
+export function updateTimes(days: string, time: Array<string>, week: Array<day>, set_or_clear: boolean) {
+  console.log("update_times")
+  let week_cp = week.map(day => ({
+  ...day,
+  times: [...day.times],
+  }));
+  if (days.includes("M")){
+    console.log("M")
+    const monday = newday(time, week_cp[0].times, set_or_clear);
+    //console.log(week[0].times)
+    week_cp[0].times = monday;
+  }if (days.includes("W")){
+    const wed = newday(time, week_cp[1].times, set_or_clear);
+    week_cp[1].times = wed;
+  } if (days.includes("F")){
+    const fri = newday(time, week_cp[2].times, set_or_clear);
+    week_cp[2].times = fri;
+  } if (days.includes("Tu")){
+    console.log("Tu")
+    const tues = newday(time, week_cp[3].times, set_or_clear);
+    week_cp[3].times = tues;
+  } if (days.includes("Th")){
+    const thurs = newday(time, week_cp[4].times, set_or_clear);
+    week_cp[4].times = thurs;  
+  }
+  //console.log(week_cp)
+  return week_cp;
+   
+}
+
+function compare_times(arr1: Array<boolean>) {
+  //console.log("here")
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] == false) {
+      return false;
+    }
+  }
+  return true; 
+}
+
+export function detectOverlap(days: string, time: Array<string>, week: Array<day>){
+  let times = calctime_in10mins(time);
+  let start_time = times[0];
+  let end_time = times[1];
+  
+  if (days.includes("M")){
+    //console.log((week[0]).times.slice(start_time, end_time))
+    
+    if (!compare_times(week[0].times.slice(start_time, end_time))) {
+      //console.log("hi")
+      return true;
+    } 
+  }if (days.includes("W")){
+    if (!compare_times(week[1].times.slice(start_time, end_time))) {
+      return true;
+    } 
+  } if (days.includes("F")){
+    if (!compare_times(week[2].times.slice(start_time, end_time))) {
+      return true;
+    } 
+  } if (days.includes("Tu")){
+      if (!compare_times(week[3].times.slice(start_time, end_time))) {
+      return true;
+    } 
+  } if (days.includes("Th")){
+      if (!compare_times(week[4].times.slice(start_time, end_time))) {
+      return true;
+      } 
+  }
+
+  return false;
+}
+
